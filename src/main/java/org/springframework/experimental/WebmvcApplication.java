3f1f4e9e87ead75dc0463e9ed6ce2692acdc08a4
Dave Syer <dsyer@vmware.com>
2024-03-06 11:32:15 +0000

    Mute the logo a bit

--- src/main/java/org/springframework/experimental/WebmvcApplication.java
+++ src/main/java/org/springframework/experimental/WebmvcApplication.java
@@ -1,14 +1,13 @@
 package org.springframework.experimental;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class WebmvcApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(WebmvcApplication.class).applicationStartup(new BufferingApplicationStartup(10)).run(args);
+		SpringApplication.run(WebmvcApplication.class, args);
 	}
 
 }
